1770333add180087a8d5d6fa9fae2f3dbf052f8e
DrovyshSV <austin@tut.by>
2023-08-07 18:13:20 +0300

    feat: add fonts, fix Text UI

--- src/shared/ui/Text/Text.tsx
+++ src/shared/ui/Text/Text.tsx
@@ -8,62 +8,90 @@ export type TextVariant = 'primary' | 'error' | 'accent';
 
 export type TextAlign = 'right' | 'left' | 'center';
 
-export type TextSize = 's' | 'm' | 'l';
+export type ParagraphsFontWeightType = 'medium' | 'roman' | 'heavy'
+
+export type TextType =
+  'h1'
+  | 'h2'
+  | 'h2_alt'
+  | 'h3'
+  | 'h3_alt'
+  | 'h4'
+  | 'h4_alt'
+  | 'h5'
+  | 'body'
+  | 'body2'
+  | 'caption'
+  | 'tiny';
 
 interface TextProps {
   className?: string;
-  title?: string;
   text?: string;
   variant?: TextVariant;
   align?: TextAlign;
-  size?: TextSize;
-  bold?: boolean;
+  type?: TextType;
+  fontWeight?: ParagraphsFontWeightType
 }
 
-type HeaderTagType = 'h1' | 'h2' | 'h3';
+type TextTagType = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'p';
 
-const mapSizeToClass: Record<TextSize, string> = {
-  s: styles.size_s,
-  m: styles.size_m,
-  l: styles.size_l,
+const mapTypeToClass: Record<TextType, string> = {
+  h1: styles.type_h1,
+  h2: styles.type_h2,
+  h2_alt: styles.type_h2Alt,
+  h3: styles.type_h3,
+  h3_alt: styles.type_h3Alt,
+  h4: styles.type_h4,
+  h4_alt: styles.type_h4Alt,
+  h5: styles.type_h5,
+  body: styles.type_body,
+  body2: styles.type_body2,
+  caption: styles.type_caption,
+  tiny: styles.type_tiny,
+
 };
 
-const mapSizeToHeaderTag: Record<TextSize, HeaderTagType> = {
-  s: 'h3',
-  m: 'h2',
-  l: 'h1',
+const mapTypeToHeaderTag: Record<TextType, TextTagType> = {
+  h1: 'h1',
+  h2: 'h2',
+  h2_alt: 'h2',
+  h3: 'h3',
+  h3_alt: 'h3',
+  h4: 'h4',
+  h4_alt: 'h4',
+  h5: 'h5',
+  body: 'p',
+  body2: 'p',
+  caption: 'p',
+  tiny: 'p',
 };
 
 export const Text = memo((props: TextProps) => {
   const {
     className,
     text,
-    title,
     variant = 'primary',
     align = 'left',
-    size = 'm',
-    bold,
+    type = 'body',
+    fontWeight,
   } = props;
 
-  const HeaderTag = mapSizeToHeaderTag[size];
-  const sizeClass = mapSizeToClass[size];
+  const TextTag = mapTypeToHeaderTag[type];
+  const typeClass = mapTypeToClass[type];
   const additionalClasses = [
     className,
     styles[variant],
     styles[align],
-    sizeClass,
+    typeClass,
   ];
-
+  if (fontWeight) {
+    additionalClasses.push(styles[fontWeight])
+  }
   return (
-    <div
-      className={classNames(
-        styles.Text,
-        { [styles.bold]: bold },
-        additionalClasses,
-      )}
-    >
-      {title && <HeaderTag className={styles.title}>{title}</HeaderTag>}
-      {text && <p className={styles.text}>{text}</p>}
-    </div>
+    <TextTag className={classNames(
+      styles.Text,
+      {},
+      additionalClasses,
+    )}>{text}</TextTag>
   );
 });
