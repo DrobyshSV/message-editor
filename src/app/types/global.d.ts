fa08cac00f2ef652e69a77523e71acc178ca62d7
DrovyshSV <austin@tut.by>
2023-08-10 18:39:17 +0300

    fix: declare module svg

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -2,14 +2,11 @@ declare module '*.scss' {
   interface IClassNames {
     [className: string]: string;
   }
+
   const classNames: IClassNames;
   export = classNames;
 }
 
 declare module '*.ttf';
 
-declare module '*.svg' {
-  // eslint-disable-next-line no-undef
-  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
-  export default content;
-}
+declare module '*.svg';
