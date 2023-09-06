82f284baf9de91b35a711a504d774a1280959588
DrovyshSV <austin@tut.by>
2023-09-06 15:53:34 +0300

    fix: add Input to App; feat: add svg Add

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,14 @@
 import { memo, Suspense } from 'react';
 
 import { AppRouter } from '@/app/router';
+import { Icon } from '@/shared/ui/Icon';
+import { Input } from '@/shared/ui/Input';
+
+import { ReactComponent as Add } from '../shared/assets/svg/Add.svg';
 
 const App = memo(() => (
   <Suspense fallback=''>
+    <Input label="sacasc" placeholder="efef" type="text" addonLeft={<Icon Svg={Add}/>} />
     <AppRouter />
   </Suspense>
 ));
