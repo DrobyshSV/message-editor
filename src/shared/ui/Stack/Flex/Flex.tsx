83ed580f7f06617588fc9a2f899a8e9d2b9d4c19
DrovyshSV <austin@tut.by>
2023-09-06 15:51:55 +0300

    fix: css gap for flex Stack

--- src/shared/ui/Stack/Flex/Flex.tsx
+++ src/shared/ui/Stack/Flex/Flex.tsx
@@ -7,7 +7,7 @@ import styles from './Flex.module.scss';
 export type FlexJustify = 'start' | 'center' | 'end' | 'between';
 export type FlexAlign = 'start' | 'center' | 'end';
 export type FlexDirection = 'row' | 'column';
-export type FlexGap = '4' | '8' | '16' | '24' | '32';
+export type FlexGap = '0.25' | '0.5' | '1' | '1.5' | '2';
 export type FlexWrap = 'nowrap' | 'wrap';
 
 const justifyClasses: Record<FlexJustify, string> = {
@@ -29,11 +29,11 @@ const directionClasses: Record<FlexDirection, string> = {
 };
 
 const gapClasses: Record<FlexGap, string> = {
-  4: styles.gap4,
-  8: styles.gap8,
-  16: styles.gap16,
-  24: styles.gap24,
-  32: styles.gap32,
+  0.25: styles.gap4,
+  0.5: styles.gap8,
+  1: styles.gap16,
+  1.5: styles.gap24,
+  2: styles.gap32,
 };
 
 type DivProps = DetailedHTMLProps<
